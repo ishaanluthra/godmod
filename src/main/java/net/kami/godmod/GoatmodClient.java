3dd46d5adc69feb39510de26f738e304b0c4fbd5
Ishaan <lishaan300@gmail.com>
2025-07-06 07:23:43 +0000

    batman

---
+++ src/main/java/net/kami/godmod/GoatmodClient.java
@@ -0,0 +1,11 @@
+package net.kami.godmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GoatmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
